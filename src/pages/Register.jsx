93c24bead34f29073dbe1581d490947fc44ed26c
bobbibao <lehoangbao1125@gmail.com>
2024-06-17 15:59:36 +0700

    Update new register form to authenticate by email

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,22 +1,64 @@
 import React, { useState } from "react";
-
-
 import { Link, useNavigate } from "react-router-dom";
 import { SectionTitle } from "../components";
 import { nanoid } from "nanoid";
 import { toast } from "react-toastify";
 
-
 const Register = () => {
+  const [step, setStep] = useState(3);
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
   const [name, setName] = useState("");
   const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [phone, setPhone] = useState("");
-  const [adress, setAdress] = useState("");
+  const [address, setAddress] = useState("");
 
   const navigate = useNavigate();
+
+  const handleEmailSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8080/send-otp", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        toast.success("OTP sent to your email.");
+        setStep(2);
+      } else {
+        toast.error("Failed to send OTP. Please try again.");
+      }
+    } catch (error) {
+      toast.error("Error: " + error.message);
+    }
+  };
+
+  const handleOtpSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8080/verify-otp", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, otp }),
+      });
+
+      if (response.ok) {
+        toast.success("OTP verified. Please fill in the registration details.");
+        setStep(3);
+      } else {
+        toast.error("Invalid OTP. Please try again.");
+      }
+    } catch (error) {
+      toast.error("Error: " + error.message);
+    }
+  };
 
   const isValidate = () => {
     let isProceed = true;
@@ -28,15 +70,15 @@ const Register = () => {
     } else if (lastname.length === 0) {
       isProceed = false;
       errorMessage = "Please enter the value in lastname field";
-    } else if (email.length === 0) {
+    } else if (gender.length === 0) {
       isProceed = false;
-      errorMessage = "Please enter the value in email field";
+      errorMessage = "Please select a gender";
     } else if (phone.length < 4) {
       isProceed = false;
       errorMessage = "Phone must be longer than 3 characters";
-    } else if (adress.length < 4) {
+    } else if (address.length < 4) {
       isProceed = false;
-      errorMessage = "Adress must be longer than 3 characters";
+      errorMessage = "Address must be longer than 3 characters";
     } else if (password.length < 6) {
       isProceed = false;
       errorMessage = "Please enter a password longer than 5 characters";
@@ -55,7 +97,7 @@ const Register = () => {
     return isProceed;
   };
 
-  const handleSubmit = (e) => {
+  const handleRegistrationSubmit = async (e) => {
     e.preventDefault();
 
     let regObj = {
@@ -63,125 +105,177 @@ const Register = () => {
       name,
       lastname,
       email,
+      gender,
       phone,
-      adress,
+      address,
       password,
       userWishlist: [],
     };
 
     if (isValidate()) {
-      fetch("http://localhost:8080/user", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(regObj),
-      })
-        .then((res) => {
+      try {
+        const response = await fetch("http://localhost:8080/user", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(regObj),
+        });
+
+        if (response.ok) {
           toast.success("Registration Successful");
           navigate("/login");
-        })
-        .catch((err) => {
-          toast.error("Failed: " + err.message);
-        });
+        } else {
+          toast.error("Registration Failed. Please try again.");
+        }
+      } catch (error) {
+        toast.error("Failed: " + error.message);
+      }
     }
   };
+
   return (
-    <div style={{flexGrow: 1}}>
+    <div style={{ flexGrow: 1 }}>
       <SectionTitle title="Register" path="Home | Register" />
       <div className="flex flex-col justify-center sm:py-12">
         <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
           <div className="bg-dark border border-gray-600 shadow w-full rounded-lg divide-y divide-gray-200">
-            <form className="px-5 py-7" onSubmit={handleSubmit}>
-              <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Tên
-              </label>
-              <input
-                type="text"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required={true}
-              />
-              <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Họ
-              </label>
-              <input
-                type="text"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                value={lastname}
-                onChange={(e) => setLastname(e.target.value)}
-                required={true}
-              />
-              <label className="font-semibold text-sm pb-1 block text-accent-content">
-                E-mail
-              </label>
-              <input
-                type="email"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required={true}
-              />
-              <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Số điện thoại
-              </label>
-              <input
-                type="tel"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                required={true}
-              />
-              <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Địa chỉ
-              </label>
-              <input
-                type="text"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                value={adress}
-                onChange={(e) => setAdress(e.target.value)}
-                required={true}
-              />
-              <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Mật khẩu
-              </label>
-              <input
-                type="password"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required={true}
-              />
-              <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Nhập lại mật khẩu
-              </label>
-              <input
-                type="password"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required={true}
-              />
-              <button
-                type="submit"
-                className="transition duration-200 bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
-              >
-                <span className="inline-block mr-2">Đăng ký</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="w-4 h-4 inline-block"
+            {step === 1 && (
+              <form className="px-5 py-7" onSubmit={handleEmailSubmit}>
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  E-mail
+                </label>
+                <input
+                  type="email"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required={true}
+                />
+                <button
+                  type="submit"
+                  className="transition duration-200 bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-              </button>
-            </form>
+                  <span className="inline-block mr-2">Send OTP</span>
+                </button>
+              </form>
+            )}
+            {step === 2 && (
+              <form className="px-5 py-7" onSubmit={handleOtpSubmit}>
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  OTP
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={otp}
+                  onChange={(e) => setOtp(e.target.value)}
+                  required={true}
+                />
+                <button
+                  type="submit"
+                  className="transition duration-200 bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+                >
+                  <span className="inline-block mr-2">Verify OTP</span>
+                </button>
+              </form>
+            )}
+            {step === 3 && (
+              <form className="px-5 py-7" onSubmit={handleRegistrationSubmit}>
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  Tên
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required={true}
+                />
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  Họ
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                  required={true}
+                />
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  Giới tính
+                </label>
+                <select
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  required={true}
+                >
+                  <option value="">Chọn giới tính</option>
+                  <option value="Male">Nam</option>
+                  <option value="Female">Nữ</option>
+                  <option value="Other">Khác</option>
+                </select>
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  Số điện thoại
+                </label>
+                <input
+                  type="tel"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  required={true}
+                />
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  Địa chỉ
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  required={true}
+                />
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  Mật khẩu
+                </label>
+                <input
+                  type="password"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required={true}
+                />
+                <label className="font-semibold text-sm pb-1 block text-accent-content">
+                  Nhập lại mật khẩu
+                </label>
+                <input
+                  type="password"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required={true}
+                />
+                <button
+                  type="submit"
+                  className="transition duration-200 bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+                >
+                  <span className="inline-block mr-2">Đăng ký</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-4 h-4 inline-block"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                  </svg>
+                </button>
+              </form>
+            )}
           </div>
           <div className="py-5 text-center">
             <Link
@@ -197,6 +291,5 @@ const Register = () => {
     </div>
   );
 };
-
 
 export default Register;
