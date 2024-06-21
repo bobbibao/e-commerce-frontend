0c144610e998cfb48e1ffed380875b5be816a867
bobbibao <lehoangbao1125@gmail.com>
2024-06-22 00:27:23 +0700

    Add about page and display to route app

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,16 +4,13 @@ import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
+    <div style={{flexGrow: 1}}>
       <SectionTitle title="About Us" path="Home | About" />
       <div className="about-content text-center max-w-2xl mx-auto mt-5">
-      <h2 className="text-6xl text-center mb-10 max-sm:text-3xl text-accent-content">We love our customers!</h2>
+      <h2 className="text-6xl text-center mb-10 max-sm:text-3xl text-accent-content">Chúng tôi yêu khách hàng!</h2>
       <p className="text-lg text-center max-sm:text-sm max-sm:px-2 text-accent-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-        obcaecati eum est commodi, quam, ut quidem deleniti quos quod temporibus
-        dicta deserunt voluptates ab! Deleniti id repellat, labore fugiat
-        obcaecati dolorem minima fugit quasi nam velit reiciendis delectus ea
-        tempora.
+        Chúng tôi luôn cố gắng mang đến cho khách hàng những sản phẩm tốt nhất, giá cả hợp lý nhất và dịch vụ chăm sóc khách hàng tốt nhất.
+        Rất vui được phục vụ quý khách hàng!
       </p>
       <Link to="/contact" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white mt-5">Contact Us</Link>
       </div>
