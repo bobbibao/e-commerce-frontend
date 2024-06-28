5d6eb17c491e0d8dd5198cfbc4e258db5111c5d6
bobbibao <lehoangbao1125@gmail.com>
2024-06-28 09:48:01 +0700

    Update calculate total cart and UI

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 
-const CartTotals = () => {
+const CartTotals = ({coupon}) => {
+  console.log(coupon)
   const { amount } = useSelector((state) => state.cart);
   const { total } = useSelector((state) => state.cart);
   const tax = total / 10;
   const shipping = 10000;
   return (
-    <div className='card bg-base-200'>
+    <div className='card bg-base-100'>
       <div className='card-body'>
         {/* SUBTOTAL */}
         <p className='flex justify-between text-xs border-b border-base-300 pb-2 text-accent-content'>
           <span>Tổng tiền hàng</span>
-          <span className='font-medium'>{ Math.round(total) }</span>
+          <span className='font-medium'>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(Math.round(total))}</span>
         </p>
         {/* SHIPPING */}
         <p className='flex justify-between text-xs border-b border-base-300 pb-2 text-accent-content'>
           <span>Phí giao hàng</span>
-          <span className='font-medium'>{ shipping }</span>
+          <span className='font-medium'>Miễn phí</span>
         </p>
         {/* Tax */}
         <p className='flex justify-between text-xs border-b border-base-300 pb-2 text-accent-content'>
-          <span>Thuế 10%</span>
-          <span className='font-medium'>{Math.round(tax)}</span>
+          <span>Giảm giá</span>
+          <span className='font-medium'>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(Math.round(
+            coupon ? total * coupon.value / 100 : 0
+          ))}</span>
         </p>
         {/* Order Total */}
         <p className='flex justify-between text-sm mt-4 pb-2 text-accent-content'>
           <span>Tổng tiền cần thanh toán</span>
-          <span className='font-medium'>{ Math.round(total + shipping + tax) }</span>
+          <span className='font-medium'> {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(Math.round(
+            total - (coupon ? total * coupon.value / 100 : 0)
+          ) )}</span>
+          
         </p>
       </div>
     </div>
